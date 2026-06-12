ed4821311c37375f546511c95aae26c56a907c0b
jam <lassouedjamel99@gmail.com>
2026-06-12 15:04:11 +0900

    chore: remove confetti easter egg and react-confetti dependency

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-
+import { useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -13,8 +13,6 @@ import {
 } from "@/components/ui/command";
 import { Button } from "./ui/button";
 import { CommandIcon } from "lucide-react";
-import Confetti from "react-confetti";
-import { useEffect, useState } from "react";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -22,8 +20,6 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = useState(false);
-  const [confetti, setConfetti] = useState(false);
-  const { width, height } = useWindowSizes();
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -39,22 +35,6 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      {confetti ? (
-        <Confetti
-          initialVelocityX={10}
-          initialVelocityY={9}
-          confettiSource={{
-            h: 0,
-            w: 0,
-            x: width / 2,
-            y: height / 2,
-          }}
-          recycle={false}
-          onConfettiComplete={() => {
-            setConfetti(false);
-          }}
-        />
-      ) : null}
       <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-background p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
@@ -83,14 +63,6 @@ export const CommandMenu = ({ links }: Props) => {
             >
               <span>Print</span>
             </CommandItem>
-            <CommandItem
-              onSelect={() => {
-                setOpen(false);
-                setConfetti(true);
-              }}
-            >
-              <span>Confetti</span>
-            </CommandItem>
           </CommandGroup>
           <CommandGroup heading="Links">
             {links.map(({ url, title }) => (
@@ -111,19 +83,3 @@ export const CommandMenu = ({ links }: Props) => {
     </>
   );
 };
-
-function useWindowSizes() {
-  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
-
-  useEffect(() => {
-    window.addEventListener("resize", updateWindowSize);
-    updateWindowSize();
-    return () => window.removeEventListener("resize", updateWindowSize);
-  }, []);
-
-  const updateWindowSize = () => {
-    setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-  };
-
-  return windowSize;
-}
